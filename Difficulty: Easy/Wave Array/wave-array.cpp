4b726eccf857cccaeecb8145b39f6536ab25e791
aash553 <122160985+aash553@users.noreply.github.com>
2025-06-09 12:29:07 +0530

    Update wave-array.cpp

--- Difficulty: Easy/Wave Array/wave-array.cpp
+++ Difficulty: Easy/Wave Array/wave-array.cpp
@@ -12,3 +12,12 @@ class Solution {
         }
     }
 };
+
+
+// better solution without using nested loop 
+
+
+// int n = arr.size();
+// for ( int i = 0 ; i<n-1 ; i+=2){
+//   swap ( arr[i], arr[i+1]);
+// }
