501d63278158c4886d46ece8a89d64cb778031cb
aash553 <122160985+aash553@users.noreply.github.com>
2025-06-09 13:27:16 +0530

    Update reversing-the-equation.cpp

--- Difficulty: Easy/Reversing the equation/reversing-the-equation.cpp
+++ Difficulty: Easy/Reversing the equation/reversing-the-equation.cpp
@@ -19,3 +19,37 @@ class Solution {
         return result;
     }
 };
+
+
+// this is using the stack operation as well 
+
+class Solution {
+public:
+    string reverseEqn(string s) {
+        stack<string> st;
+        string temp = "";
+
+        // Traverse from left to right
+        for (int i = 0; i < s.length(); i++) {
+            if (isdigit(s[i])) {
+                temp += s[i]; // accumulate digits
+            } else {
+                st.push(temp);                   // push number
+                st.push(string(1, s[i]));        // push operator as string
+                temp = "";
+            }
+        }
+
+        // Push the last number to the stack
+        st.push(temp);
+
+        // Build the result from stack (reverses the expression)
+        string result = "";
+        while (!st.empty()) {
+            result += st.top();
+            st.pop();
+        }
+
+        return result;
+    }
+};
