df4771bd1f72c1dab6484cb909f089998a2d5831
aash553 <122160985+aash553@users.noreply.github.com>
2025-06-16 18:17:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Kth Smallest/kth-smallest.cpp
@@ -0,0 +1,17 @@
+// User function template for C++
+
+class Solution {
+  public:
+    // arr : given array
+    // k : find kth smallest element and return using this function
+    int kthSmallest(vector<int> &arr, int k) {
+        priority_queue<int>maxheap;
+        for(int i =0;i<arr.size();i++){
+            maxheap.push(arr[i]);
+        if(maxheap.size()>k){
+            maxheap.pop();
+        }
+        }
+        return maxheap.top();
+    }
+};
