8211f57124035324c0cb7e35a800753d2ff3f8db
aash553 <122160985+aash553@users.noreply.github.com>
2025-06-16 17:44:27 +0530

    Time: 11 ms (25.13%), Space: 7.9 MB (44.95%) - LeetHub

--- 0668-kth-smallest-number-in-multiplication-table/0668-kth-smallest-number-in-multiplication-table.cpp
+++ 0668-kth-smallest-number-in-multiplication-table/0668-kth-smallest-number-in-multiplication-table.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
 
-
-    int issmall(int m , int n , int k ,int mid){
-        int cnt = 0;
-        for ( int i=1;i<=m;i++){
-            cnt+=min(mid/i,n);
+      int cnt (int m,int n , int k ,int mid){
+            int cnt = 0;
+            for ( int i=1;i<=m;i++){
+                cnt+=min(mid/i,n);
+            }
+            return cnt>=k;
         }
-        return cnt>=k;
-    }
 
     int findKthNumber(int m, int n, int k) {
-        int low = 0;
-        int high = m*n;
-        while(low<high){
-            int mid = low+(high-low)/2;
-            if(issmall(m,n,k,mid)){
-                high=mid;
-            }
-            else{
-                low=mid+1;
-            }
+      int low = 0;
+      int high = m*n;
+      while(low<high){
+        int mid = low+(high-low)/2;
+        if(cnt(m,n,k,mid)){
+            high=mid;
+        }else{
+            low =mid +1;
         }
-        return low;
+      }
+      return low;
     }
 };
