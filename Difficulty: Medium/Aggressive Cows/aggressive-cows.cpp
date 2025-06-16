d4c5c2334c26b4916fae68dec755392a95eec9ee
aash553 <122160985+aash553@users.noreply.github.com>
2025-06-16 19:25:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Aggressive Cows/aggressive-cows.cpp
+++ Difficulty: Medium/Aggressive Cows/aggressive-cows.cpp
@@ -3,47 +3,42 @@
 class Solution {
   public:
   
-  
-   bool ispossible(vector<int> &stalls ,int k,  int mid ){
-       int cowcnt = 1;
-       int lastpos = stalls[0];
-       for ( int i = 0;i<stalls.size();i++){
-           
-       if(stalls[i]-lastpos >= mid){
-           cowcnt++;
-           if(cowcnt == k){
-               return true;
-           }
-           lastpos=stalls[i];
-       }
-       }
-       return false;
-   }
-
-    int aggressiveCows(vector<int> &stalls, int k) {
-        
-    sort(stalls.begin(),stalls.end());
-
-      int s = 0;
-      int ans = -1;
-      int maxi = -1;
-      for(int i =0;i<stalls.size();i++){
-          maxi= max(maxi,stalls[i]);
-      }
-      int e = maxi;
+  bool ispossible(vector<int> & stalls, int k , int mid  ){
+      int cowcnt = 1; 
+      int lastpow  = stalls[0];
       
-      while(s<=e){
-          int mid = s+ (e-s)/2;
-          
-          if(ispossible(stalls,k,mid)){
-              ans = mid ;
-              s = mid + 1;
-          }else{
-              e = mid -1 ;
+      for(int i =0;i<stalls.size();i++){
+          if(stalls[i]-lastpow>=mid){
+             cowcnt++;
+             if(cowcnt==k){
+                 return true ;
+             }
+             lastpow= stalls[i];
           }
       }
+      return false;
+  }
+
+    int aggressiveCows(vector<int> &stalls, int k) {
+
+      sort(stalls.begin(),stalls.end());
       
-      return ans ;
-        
+      int s = 0;
+      int maxi = -1;
+      for(int i=0;i<stalls.size();i++){
+          maxi = max(maxi,stalls[i]);
+      }
+      int e = maxi;
+      int ans = -1;
+      while(s<=e){
+          int mid = s+(e-s)/2;
+          if(ispossible(stalls,k,mid)){
+              ans = mid;
+              s = mid+1;
+          }else{
+              e = mid-1;
+          }
+      }
+      return ans;
     }
 };
