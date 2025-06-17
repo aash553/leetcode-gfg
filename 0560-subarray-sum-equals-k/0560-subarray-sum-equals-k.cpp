6fc775e65b53c592c60336e000df7612ba7347f8
aash553 <122160985+aash553@users.noreply.github.com>
2025-06-17 19:36:06 +0530

    Time: 93 ms (24.61%), Space: 54 MB (5.35%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int prefix = 0;
+        int cnt = 0;
+        map<int,int>mpp;
+        mpp[0]=1;
+        for ( int i =0;i<nums.size();i++){
+            prefix+=nums[i];
+            int rem = prefix-k;
+            cnt+=mpp[rem];
+            mpp[prefix]+=1;
+        }
+        return cnt;
+    }
+};
