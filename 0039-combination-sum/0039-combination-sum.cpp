9024332d55370482107263ea0110711420857ebf
aash553 <122160985+aash553@users.noreply.github.com>
2025-06-18 15:25:32 +0530

    Time: 3 ms (56.11%), Space: 14 MB (90.97%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
 
-    void fuz(int ind , int target , vector<int>&arr , vector<int> &ds , vector<vector<int>> &ans){
-        if(ind == arr.size()){
-            if(target == 0 ){
-                ans.push_back(ds);
-            }
+    void solve(int ind , int target , vector<int>& arr , vector<int>&ds , vector<vector<int>>&ans){
+        if(ind==arr.size()){
+        if(target==0){
+            ans.push_back(ds);
+        }
             return;
         }
-        //to pick
-        if(arr[ind]<=target){
+
+            if(arr[ind]<=target) {
             ds.push_back(arr[ind]);
-            fuz(ind,target-arr[ind],arr,ds,ans);
+            solve(ind,target-arr[ind],arr,ds,ans);
             ds.pop_back();
-        }
-        //for not picking 
-        fuz(ind+1,target,arr,ds,ans);
+            }
+            solve(ind+1,target,arr,ds,ans);
     }
 
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>>ans;
-        vector<int>ds;
-
-        fuz(0,target,candidates,ds,ans);
-        return ans;
+        vector<int> ds ;
+        solve (0,target,candidates,ds,ans);
+        return ans ;
     }
 };
