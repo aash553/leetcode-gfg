96802fe604419435efc551586c561fe793d74ab6
aash553 <122160985+aash553@users.noreply.github.com>
2025-06-23 15:14:07 +0530

    Time: 3 ms (68.53%), Space: 65.1 MB (41.79%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -3,18 +3,15 @@ public:
     int findDuplicate(vector<int>& nums) {
         int slow = 0;
         int fast = 0;
-
         do{
-            slow = nums[slow];
-            fast = nums[nums[fast]];
+        slow = nums[slow];
+        fast= nums[nums[fast]];
         }while(slow!=fast);
 
-        //move the pointer to the start and find until they meet again 
-
-        slow = 0;
+        slow=0; 
         while(slow!=fast){
             slow=nums[slow];
-            fast = nums[fast];
+            fast=nums[fast];
         }
         return slow;
     }
