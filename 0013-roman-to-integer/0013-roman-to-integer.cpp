af7f8ecca72035961034135f1e2f6dff54c08734
aash553 <122160985+aash553@users.noreply.github.com>
2025-06-30 12:28:38 +0530

    Time: 0 ms (100%), Space: 10.1 MB (84.54%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
 
-    int chartonum(char a){
-        switch(a){
+ int solve(char a){
+    switch(a){
             case 'I' : return 1;
             case 'V' : return 5;
             case 'X' : return 10;
@@ -11,19 +11,18 @@ public:
             case 'D' : return 500;
             case 'M' : return 1000;
             default : return 0;
-        }
     }
+ }
 
     int romanToInt(string s) {
-        
-        int result = 0;
-        for ( int i =0;i<s.length();i++){
-            if(i+1<s.length() && chartonum(s[i]) < chartonum(s[i+1])){
-            result -= chartonum(s[i]);
-        }else{
-            result +=chartonum(s[i]);
-        }
+        int result=0;
+        for(int i =0;i<s.length();i++){
+            if(i+1<s.length() && solve(s[i])< solve(s[i+1]) ){
+                result-=solve(s[i]);
+            }else{
+                result+=solve(s[i]);
             }
-            return result;
+        }
+        return result;
     }
 };
