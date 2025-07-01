66342121d79cec2ee071275906b12cd5136e7c50
Aashish <122160985+aash553@users.noreply.github.com>
2025-07-01 10:47:04 +0530

    Time: 0 ms (100%), Space: 10.1 MB (45.77%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        
-        if(s1.size()>s2.size()) return false;
 
-        vector<int>freq1(26,0) , freq2(26,0);
+        if(s1.length()>s2.length()) return false;
 
-        for ( int i=0;i<s1.size();i++){
+        vector<int> freq1(26,0) , freq2(26,0);
+
+        for(int i =0; i<s1.length();i++){
+
             freq1[s1[i]-'a']++;
             freq2[s2[i]-'a']++;
         }
 
-
-        for (int i =s1.size();i <s2.size();i++){
+        for(int i = s1.length(); i<s2.length();i++){
             if(freq1==freq2) return true;
+
             freq2[s2[i]-'a']++;
-            freq2[s2[i-s1.size()]-'a']--;
+            freq2[s2[i-s1.length()]-'a']--;
+
         }
-        return freq1==freq2;
+        return (freq1==freq2);
     }
 };
