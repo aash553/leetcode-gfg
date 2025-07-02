d54e6320beea0cdf1c3ba9f67f25bd0b8bbce048
Aashish <122160985+aash553@users.noreply.github.com>
2025-07-03 01:58:37 +0530

    Time: 3 ms (98.44%), Space: 41.3 MB (99.24%) - LeetHub

--- 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.cpp
@@ -3,17 +3,24 @@ public:
 
     int solve(vector<int>&nums, int k ){
           int n = nums.size();
-        unordered_map<int,int>mpp;
+       // unordered_map<int,int>mpp;
+       int freq[100005]={0};
         int l=0;
         int r=0;
         int cnt = 0;
+        int discnt=0;
         while(r<n){
-            mpp[nums[r]]++;
-            while(mpp.size()>k){
-                mpp[nums[l]]--;
-                if(mpp[nums[l]]==0)
-                mpp.erase(nums[l]);
-                l++;
+            //mpp[nums[r]]++;
+            if(freq[nums[r]]==0){
+                discnt++;
+            }
+            freq[nums[r]]++;
+            while(discnt>k){
+                freq[nums[l]]--;
+                if(freq[nums[l]]==0){
+                discnt--;
+                }
+            l++;
             }
 
             cnt = cnt+(r-l+1);
