018d06d1f8b9cb4d20a0b73a93c01b07f4966cb7
Aashish <122160985+aash553@users.noreply.github.com>
2025-07-05 21:37:01 +0530

    Time: 59 ms (48.28%), Space: 109.8 MB (30.8%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
 
-    void help(int ind , int &n, int k , vector<int>&ds, vector<vector<int>>&ans){
+
+    void solve(int ind , int &n , int k , vector<vector<int>>&ans, vector<int>&ds){
+
         if(k==0){
             ans.push_back(ds);
             return;
@@ -9,17 +11,18 @@ public:
 
         if(ind>n) return;
         ds.push_back(ind);
-        
-        help(ind+1,n,k-1,ds,ans);
+        solve(ind+1,n,k-1,ans,ds);
         ds.pop_back();
+        solve(ind+1,n,k,ans,ds);
 
-        help(ind+1,n,k,ds,ans);
     }
 
     vector<vector<int>> combine(int n, int k) {
+
         vector<vector<int>>ans;
         vector<int>ds;
-        help(1,n,k,ds,ans);
+
+         solve(1,n,k,ans,ds);
         return ans;
     }
 };
