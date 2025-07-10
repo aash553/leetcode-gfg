17eb22dea7d85885853296e1ce0e4016a7f16dec
Aashish <122160985+aash553@users.noreply.github.com>
2025-07-10 14:10:26 +0530

    Time: 0 ms (100%), Space: 10 MB (9.44%) - LeetHub

--- 1127-last-stone-weight/1127-last-stone-weight.cpp
+++ 1127-last-stone-weight/1127-last-stone-weight.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
-         priority_queue<int> maxHeap(stones.begin(), stones.end());
+        while(stones.size()>1){
+        sort(stones.begin(),stones.end());
+            int a = stones.back() ; stones.pop_back();
+            int b = stones.back() ; stones.pop_back();
 
-        while (maxHeap.size() > 1) {
-            int stone1 = maxHeap.top(); maxHeap.pop(); // heaviest
-            int stone2 = maxHeap.top(); maxHeap.pop(); // second heaviest
-
-            if (stone1 != stone2) {
-                maxHeap.push(stone1 - stone2);
-            }
-            // If stone1 == stone2, both are destroyed, do nothing
+            stones.push_back(abs(a-b));
         }
-
-        return maxHeap.empty() ? 0 : maxHeap.top();
+        return stones[0];
     }
 };
