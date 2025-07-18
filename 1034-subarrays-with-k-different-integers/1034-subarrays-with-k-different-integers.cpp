074e7c4afdf19377737c9692aa21f34cd2981846
Aashish <122160985+aash553@users.noreply.github.com>
2025-07-18 15:07:30 +0530

    Time: 84 ms (53.97%), Space: 59.2 MB (25.03%) - LeetHub

--- 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.cpp
@@ -1,34 +1,27 @@
 class Solution {
 public:
 
-    int solve(vector<int>&nums, int k ){
-          int n = nums.size();
-       // unordered_map<int,int>mpp;
-       int freq[100005]={0};
-        int l=0;
-        int r=0;
-        int cnt = 0;
-        int discnt=0;
+int solve(vector<int>nums , int k){
+   unordered_map<int,int> mpp;
+
+        int l =0;
+        int r = 0;
+        int n = nums.size();
+        int cnt = 1;
+
         while(r<n){
-            //mpp[nums[r]]++;
-            if(freq[nums[r]]==0){
-                discnt++;
-            }
-            freq[nums[r]]++;
-            while(discnt>k){
-                freq[nums[l]]--;
-                if(freq[nums[l]]==0){
-                discnt--;
-                }
+            mpp[nums[r]]++;
+        while(mpp.size()>k){
+            mpp[nums[l]]--;
+            if(mpp[nums[l]]==0)
+            mpp.erase(nums[l]);
             l++;
-            }
-
-            cnt = cnt+(r-l+1);
-            r=r+1;
-
+        }
+        r=r+1;
+        cnt += r-l+1;
         }
         return cnt;
-    }
+}
 
     int subarraysWithKDistinct(vector<int>& nums, int k) {
         return solve(nums,k) - solve(nums,k-1);
