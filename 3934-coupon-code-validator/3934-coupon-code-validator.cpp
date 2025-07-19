efdb7f15dd799d8b6e8cc1f2890187948480b5a3
Aashish <122160985+aash553@users.noreply.github.com>
2025-07-19 19:56:15 +0530

    Time: 7 ms (60.86%), Space: 69.9 MB (40.02%) - LeetHub

--- 3934-coupon-code-validator/3934-coupon-code-validator.cpp
+++ 3934-coupon-code-validator/3934-coupon-code-validator.cpp
@@ -1,51 +1,56 @@
 class Solution {
 public:
 
-    bool validcode(const string &s){
-        if (s.empty()) return false;
-        for ( char ch : s ){
-            if(!(isalnum(ch) || ch == '_' )){
+    bool validcode( const string & s){
+        if(s.empty()) return false;
+        for(char ch : s){
+            if(!(isalnum(ch) || ch == '_')){
                 return false;
             }
         }
         return true;
     }
 
-   bool validbusiness(const string &s){
-    return s == "electronics" || s == "grocery" || s == "pharmacy" || s == "restaurant";
-   } 
-
-   bool validactive(bool status){
-    return status == true;
-   }
-
-    bool isvalid(const string& codeStr, const string& business, bool isActiveStatus){
-    return validcode(codeStr) && validbusiness(business) && validactive(isActiveStatus);
+    bool validbusiness(const string &s){
+        return s == "electronics" || s == "grocery" || s == "pharmacy" || s =="restaurant" ;
     }
-   
+
+    bool validactive(bool status){
+
+    return status == true;
+    }
+
+    
+    bool isvalid(const string &abc , const string & def , bool ghi){
+        return validcode(abc) && validbusiness(def) && validactive(ghi);
+    }
+
 
     vector<string> validateCoupons(vector<string>& code, vector<string>& businessLine, vector<bool>& isActive) {
+        vector<string>result;
+        vector<pair<string,string>>validcoupons;
 
-        vector<pair<string, string>> validCoupons;
-        unordered_map<string, int> order = {
-            {"electronics", 0},
-            {"grocery", 1},
-            {"pharmacy", 2},
-            {"restaurant", 3}
+        unordered_map<string,int>order = {
+            {"electronics",0},
+            {"grocery",1},
+            {"pharmacy",2},
+            {"restaurant",3}
         };
+
         int n = code.size();
-        for (int i = 0; i < n; i++) {
-            if (isvalid(code[i], businessLine[i], isActive[i])) {
-                validCoupons.push_back({businessLine[i], code[i]});
+        for(int i =0;i<n;i++){
+            if(isvalid(code[i],businessLine[i],isActive[i])){
+                validcoupons.push_back({businessLine[i],code[i]});
             }
         }
-         sort(validCoupons.begin(), validCoupons.end(), [&](auto &a, auto &b) {
-            if (order[a.first] != order[b.first])
+
+        sort(validcoupons.begin(), validcoupons.end() ,[&] (auto &a, auto &b){
+            if(order[a.first]!=order[b.first])
                 return order[a.first] < order[b.first];
-            return a.second < b.second; 
+            return a.second < b.second;
         });
-        vector<string> result;
-        for (auto &entry : validCoupons) {
+
+        for(auto &entry : validcoupons){
             result.push_back(entry.second);
         }
         return result;
