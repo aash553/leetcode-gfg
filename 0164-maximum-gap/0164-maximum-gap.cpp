67684ff42e218fe347ce44038d9d71f18f6116c3
Aashish <122160985+aash553@users.noreply.github.com>
2025-07-24 13:41:02 +0530

    Time: 15 ms (88.63%), Space: 91.7 MB (32.82%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.cpp
+++ 0164-maximum-gap/0164-maximum-gap.cpp
@@ -1,59 +1,63 @@
 class Solution {
 public:
     int maximumGap(vector<int>& nums) {
-        // find the min and max  element 
         int n = nums.size();
-        if ( n<2) return 0;
+
+        //edge case very important
+        if (nums.size() < 2) return 0;
 
         int mini = nums[0];
         int maxi = nums[0];
 
-        for (int i =0;i<n;i++){
-            if(nums[i]<mini) mini = nums[i];
-            if(nums[i]>maxi) maxi = nums[i];
+        //finding mini and maxi element
+
+        for(int i = 0;i<n;i++){
+           if(nums[i]<mini) mini = nums[i];
+           if(nums[i]>maxi) maxi = nums[i];
         }
-        if (mini==maxi) return 0;
 
-        //get the bucketsize adn bucketcount (remeber always n-1 size for n-2 elements)
+        //another very important edge case
+        if (maxi == mini) return 0;
 
-        int bucketsize = (maxi - mini + (n-2))/(n-1);
-        int bucketcount = (maxi - mini)/bucketsize+1;
+        //calculation the bucketsize and bucketcount 
+        int bucketsize = ((maxi-mini)+(n-2))/(n-1);
+        int bucketcount = (maxi-mini)/bucketsize+1;
 
-    //create the vectors to fill in 
+        //create buckets 
 
-    vector<int> bucketmin(bucketcount , INT_MAX);
-    vector<int> bucketmax(bucketcount , INT_MIN);
-    vector<bool> bucketused(bucketcount , false);
+        vector<int>bucketmin(bucketcount , INT_MAX);
+        vector<int>bucketmax(bucketcount, INT_MIN);
+        vector<bool> bucketused (bucketcount , false);
 
-    //start filling the elements
 
-    for(int i =0;i<n;i++){
-        int num= nums[i];
-        int ind = (num-mini)/bucketsize;
+        //distribution of elements accordingly
+        for(int i =0;i<n;i++){
+            int num = nums[i];
+            int ind = (num-mini)/bucketsize;
 
-        if(!bucketused[ind]){
-            bucketmin[ind]=num;
-            bucketmax[ind]=num;
-            bucketused[ind]=true;   
-        }else{
-            if(num<bucketmin[ind])bucketmin[ind] = num;
-            if(num>bucketmax[ind]) bucketmax[ind] = num;
+            if(!bucketused[ind]){
+                bucketmin[ind]=num;
+                bucketmax[ind]=num;
+                bucketused[ind] = true;
+            }else{
+                if(num<bucketmin[ind]) bucketmin[ind] = num;
+                if(num>bucketmax[ind]) bucketmax[ind] = num;
+            }
         }
+
+        //computing maximum gap
+
+        int maxigap = 0;
+        int prev = mini;
+
+        for(int i =0;i<bucketcount ; i++){
+            if(!bucketused[i]) continue;
+
+            int gap = bucketmin[i] - prev;
+           if(gap>maxigap ) maxigap = gap;
+
+           prev = bucketmax[i];
+         }
+         return maxigap;
     }
-
-    //compute maxi gap now 
-
-    int maxgap =0;
-    int prev= mini;
-
-    for ( int i=0;i<bucketcount;i++){
-        if(!bucketused[i])continue;
-
-        int gap = bucketmin[i] - prev;
-        if(gap>maxgap) maxgap = gap;
-
-        prev = bucketmax[i];
-    }
-    return maxgap;
-}
 };
