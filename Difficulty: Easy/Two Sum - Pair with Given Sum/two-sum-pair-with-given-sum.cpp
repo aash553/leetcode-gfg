7fe99bc7ecc3e5998f0ad5e2980b1abb4718c619
Aashish <122160985+aash553@users.noreply.github.com>
2025-07-24 19:39:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Two Sum - Pair with Given Sum/two-sum-pair-with-given-sum.cpp
+++ Difficulty: Easy/Two Sum - Pair with Given Sum/two-sum-pair-with-given-sum.cpp
@@ -1,12 +1,12 @@
 class Solution {
   public:
     bool twoSum(vector<int>& arr, int target) {
-        // code here
+        
         unordered_set<int>seen;
         
-        for(int i =0;i<arr.size();i++){
-            int compli = target - arr[i];
-            if(seen.count(compli)) return true;
+        for(int i=0;i<arr.size();i++){
+            int contri = target-arr[i];
+            if(seen.count(contri)) return true;
             seen.insert(arr[i]);
         }
         return false;
