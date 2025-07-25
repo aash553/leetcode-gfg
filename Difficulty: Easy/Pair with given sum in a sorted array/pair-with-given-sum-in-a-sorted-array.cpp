cd73cf68f4bb9b83b8941316fc448b8a09f177bd
Aashish <122160985+aash553@users.noreply.github.com>
2025-07-25 06:59:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Pair with given sum in a sorted array/pair-with-given-sum-in-a-sorted-array.cpp
+++ Difficulty: Easy/Pair with given sum in a sorted array/pair-with-given-sum-in-a-sorted-array.cpp
@@ -2,42 +2,38 @@
 class Solution {
   public:
     int countPairs(vector<int> &arr, int target) {
-        int ans = 0;
-        int st = 0 ;
-         int end = arr.size()-1;
-         
-         while(st<=end){
-         int curr = arr[st]+arr[end];
-         if(curr>target){
-             end--;
-         }else if (curr<target){
-             st++;
-         }else{
-         
-         int ele1=arr[st] , ele2 = arr[end] , cnt1= 0 , cnt2=0 ;
-         
-         while(st<=end && arr[st]==ele1){
-             st++;
-             cnt1++;
-         }
-         
-         while(st<=end && arr[end]==ele2){
-             end--;
-             cnt2++;
-         }
-         
-         if(ele1==ele2){
-             ans += (cnt1*(cnt1-1))/2;
-         }
-         
-         else{
-             ans+=cnt1*cnt2;
-         }
-             
-         }
-             
-         }
-         return ans;
-         
+        int st = 0;
+        int ans =0;
+        int end = arr.size()-1;
+        
+        while(st<=end){
+            int curr = arr[st] + arr[end];
+            if(curr>target){
+                end--;
+            }
+            else if(curr<target){
+                st++;
+            }
+            else{
+                int ele1 =arr[st] ; int cnt1 =0 ; int ele2=arr[end] ; int cnt2=0;
+                
+                while(st<=end && arr[st] == ele1){
+                    cnt1++;
+                    st++;
+                }
+                
+                while(st<=end && arr[end]== ele2){
+                    cnt2++;
+                    end--;
+                }
+                if(ele1==ele2){
+                    ans += (cnt1*(cnt1-1)/2);
+                }
+                else{
+                    ans += cnt1*cnt2;
+                }
+            }
+        }
+        return ans;
     }
 };
