ef2b465c76eb90d7b59e0386f4c8a5c233589e2a
Aashish <122160985+aash553@users.noreply.github.com>
2025-07-31 14:02:02 +0530

    Time: 3 ms (42.24%), Space: 20.3 MB (45.32%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,8 +1,9 @@
 class Solution {
-public:
 
-    void dfs(int node,vector<int>adjLs[],vector<int>&vis){
-        vis[node]=1;
+private : 
+
+    void dfs(int node , vector<int>adjLs[] , vector<int>&vis){
+        vis[node] =1;
         for(auto it : adjLs[node]){
             if(!vis[it]){
                 dfs(it,adjLs,vis);
@@ -11,22 +12,24 @@ public:
     }
 
 
+
+public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        //we are give ne a matrix we will ocnvert it into adjls
+        
+        //lets convert matrix into adjeceny list
         int n = isConnected.size();
+        int cnt = 0;
         vector<int>adjLs[n];
-        for(int i = 0 ;i<n;i++){
-            for(int j=0;j<n;j++){
+        for(int i =0;i<n;i++){
+            for(int j= 0 ; j<n;j++){
                 if(isConnected[i][j] == 1 && i != j){
                     adjLs[i].push_back(j);
-                   // adjLs[j].push_back(i);
                 }
             }
         }
-
-       vector<int>vis(n,0);
-        int cnt = 0;
-        for(int i =0;i<n;i++){
+        //we create visisted list
+        vector<int>vis(n,0);
+        for(int i=0;i<n;i++){
             if(!vis[i]){
                 cnt++;
                 dfs(i,adjLs,vis);
