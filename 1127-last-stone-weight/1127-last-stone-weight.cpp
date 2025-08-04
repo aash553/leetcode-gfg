cbac325e85e260416172a4c195156c79757d8378
Aashish <122160985+aash553@users.noreply.github.com>
2025-08-04 12:11:43 +0530

    Time: 0 ms (100%), Space: 10 MB (9.28%) - LeetHub

--- 1127-last-stone-weight/1127-last-stone-weight.cpp
+++ 1127-last-stone-weight/1127-last-stone-weight.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
-        priority_queue<int>pq;
-            for(int &stone : stones){
-                pq.push(stone);
-            }
-        while(pq.size()>1){
-                int a = pq.top();
-                pq.pop();
-
-                int b= pq.top();
-                pq.pop();
-
-                pq.push(abs(a-b));
+        
+        //pushing all elements of array in to maxheap
+        int n = stones.size();
+        priority_queue<int>q;
+        for(auto it : stones){
+            q.push(it);
         }
-        return pq.top();
+        while(q.size()>1){
+            int y = q.top();
+            q.pop();
+            int x = q.top();
+            q.pop();
+
+            if(x!=y){
+                q.push(abs(y-x));
+            }
+        }
+        return q.empty() ? 0 : q.top();
     }
 };
