2d33ed7610870f3078c72608da4aedd1a0c8ace1
Aashish <122160985+aash553@users.noreply.github.com>
2025-08-05 12:08:53 +0530

    Time: 10 ms (91.45%), Space: 47.2 MB (62.86%) - LeetHub

--- 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
@@ -1,37 +1,36 @@
 class Solution {
 public:
     int sumSubarrayMins(vector<int>& arr) {
-        int M = 1e9+7;
-        stack<int>st;
+        int m = 1e9+7;
         int n = arr.size();
-         vector<int> pse(n), nse(n);
+        stack<int>st;
+        vector<int>nse(n),pse(n);
 
-        for(int i =n-1; i>=0; i--){
-            while(!st.empty() && arr[st.top()]>=arr[i]){
+        for(int i =n-1;i>=0;i--){
+            while(!st.empty() && arr[st.top()] >=  arr[i]){
                 st.pop();
             }
-            nse[i]= st.empty() ? n : st.top();
+            nse[i] =  st.empty() ? n : st.top();
             st.push(i);
         }
 
         while(!st.empty()) st.pop();
 
-        for(int i=0;i<n;i++){
-            while(!st.empty() && arr[st.top()]>arr[i]){
+        for(int i =0;i<n;i++){
+            while(!st.empty() && arr[st.top()] > arr[i]){
                 st.pop();
             }
             pse[i] = st.empty() ? -1 : st.top();
             st.push(i);
         }
 
-        long long  total = 0;
-        
-        for(int i=0;i<n;i++){
-            long long  left = i-pse[i];
-            long long  right = nse[i]-i;
+        long long sum = 0;
+        for(int i =0;i<n;i++){
+            long long left = i-pse[i];
+            long long right = nse[i]-i;
 
-            total = (total+(right*left*1LL*arr[i])%M)%M;
+            sum = (sum+(right*left*1LL*arr[i])%m)%m;
         }
-        return total;
+        return sum;
     }
 };
