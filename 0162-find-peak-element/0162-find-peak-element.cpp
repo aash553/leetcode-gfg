c2b90bfb8cc42f9def57c0410e6a06ddbf59a3ab
Aashish <122160985+aash553@users.noreply.github.com>
2025-08-08 13:38:37 +0530

    Time: 0 ms (100%), Space: 12.5 MB (32.33%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -4,15 +4,14 @@ public:
 
         int s = 0;
         int e = nums.size()-1;
-        int mid = s+(e-s)/2;
         while(s<e){
-             mid = s+(e-s)/2;
+            int  mid = s+(e-s)/2;
             if(nums[mid] > nums[mid+1]){
                 e =mid;
             }else{
                 s= mid+1;
             }
         }
-        return mid;
+        return s;
     }
 };
