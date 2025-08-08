3bed3a7d427bc6c058f76484922e05dfa31f782d
Aashish <122160985+aash553@users.noreply.github.com>
2025-08-08 12:24:14 +0530

    Time: 0 ms (100%), Space: 17.5 MB (86.33%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,56 +1,11 @@
 class Solution {
 public:
-
-    int findfirst(vector<int>& nums, int target){
-
-        int n = nums.size();
-
-        int s= 0;
-        int e = n-1;
-        int ans = -1;
-        while(s<=e){
-            int mid = s+(e-s)/2;
-            if(nums[mid] == target){
-                ans = mid;
-                e = mid - 1 ; // keep searching on the left 
-            }
-            else if(nums[mid] < target){
-                s= mid+1;
-            }
-            else{
-                e = mid -1;
-            }
-        }
-        return ans;
-    }
-
-
-    int findlast(vector<int>& nums, int target){
-         int n = nums.size();
-
-        int s= 0;
-        int e = n-1;
-        int ans = -1;
-        while(s<=e){
-            int mid = s+(e-s)/2;
-            if(nums[mid] == target){
-                ans = mid;
-                s = mid + 1; // keep searching in the right
-            }else if(nums[mid] < target){
-                s = mid + 1;
-            }
-            else{
-                e = mid -1;
-            }
-        } 
-        return ans;
-    }
-
-
-
     vector<int> searchRange(vector<int>& nums, int target) {
-        int first= findfirst(nums,target);
-        int last = findlast(nums,target);
-        return {first,last};
+        int lb = lower_bound(nums.begin(),nums.end(),target) - nums.begin();
+        if(lb == nums.size() || nums[lb] != target){
+            return {-1,-1};
+        }
+        int ub = upper_bound(nums.begin(),nums.end(),target) - nums.begin()-1;
+        return {lb,ub};
     }
 };
