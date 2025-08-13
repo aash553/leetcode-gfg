9c660ce724b2bc43281b23b39ba7ec29b2d78ad2
Aashish <122160985+aash553@users.noreply.github.com>
2025-08-13 18:25:28 +0530

    Time: 0 ms (100%), Space: 15.7 MB (67.44%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-
-        //find the break point 
-        int n=nums.size();
+        //finding the break point 
         int ind = -1;
-        for(int i =n-2;i>=0;i--){
-            if(nums[i]<nums[i+1]){
+        int n = nums.size();
+        for(int i =n-2 ; i >=0;i--){
+            if (nums[i] < nums[i+1] ){
                 ind = i;
                 break;
             }
         }
-
-        //if there is no break point reverse the whole array 
-
         if(ind == -1){
             reverse(nums.begin(),nums.end());
             return;
         }
 
-        //after finding the breakpoint find the next element which is just bigger than ind element
-
-        for(int i=n-1;i>ind;i--){
-            if(nums[i]>nums[ind]){
+        //find the element closest to ind
+        for(int i = n-1 ;i>ind ;i--){
+            if(nums[i] > nums[ind]){
                 swap(nums[i],nums[ind]);
                 break;
             }
         }
 
-        // after that reverse the whole 
-
-        reverse(nums.begin()+ind+1, nums.end());
+        //reverse the remaining half
+        reverse(nums.begin()+ind+1,nums.end());
     }
 };
