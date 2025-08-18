a7101b745319179164799340ad86695d4645f275
Aashish <122160985+aash553@users.noreply.github.com>
2025-08-18 19:20:21 +0530

    Time: 0 ms (100%), Space: 9.6 MB (73.97%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int n = s.length();
-        if(s.length() != t. length()) return false;
 
-        vector<int> count(26,0) ;
+        //int n = s.length();
 
-        for(int i =0;i<n;i++){
+        if(s.length() != t.length()) return false;
 
+        
+        vector<int> count(26,0);
+
+        for(int i =0;i<s.length() ; i++){
             count[s[i]-'a']++;
             count[t[i]-'a']--;
         }
-
-
-        for(int i=0; i<26 ; i++){
-        if(count[i]!=0)
-        return false;
+        for(int i =0;i<26 ;i++){
+            if(count[i] !=0)
+            return false;
         }
         return true;
     }
