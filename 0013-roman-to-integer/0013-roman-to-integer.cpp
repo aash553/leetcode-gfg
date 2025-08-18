78106ac81b8065df77de2dc5a79c81e995ee00b5
Aashish <122160985+aash553@users.noreply.github.com>
2025-08-18 18:44:53 +0530

    Time: 0 ms (100%), Space: 9.9 MB (99.06%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
 
-      int solve(char a){
-            switch (a) {
+    int roman(char a){
+        switch(a){
             case 'I' : return 1;
             case 'V' : return 5;
             case 'X' : return 10;
@@ -12,18 +12,17 @@ public:
             case 'M' : return 1000;
              default : return 0;
         }
-        }
-
+    }
     int romanToInt(string s) {
         int result = 0;
         int n = s.length();
-        for(int i =0;i<n;i++){
-            if(i+1<s.length() && solve(s[i])< solve(s[i+1]) ){
-                result -= solve(s[i]);
-            }
-            else{
-                result+=solve(s[i]);
-            }
+        for(int i =0;i<s.length();i++){
+        if(i+1< n && roman(s[i]) < roman(s[i+1])){
+            result -= roman(s[i]);
+        }
+        else{
+            result +=roman(s[i]);
+        }
         }
         return result;
     }
