804cef0e1dc74e7a17e25ba735a80966569c3866
Aashish <122160985+aash553@users.noreply.github.com>
2025-08-22 00:38:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Tree Boundary Traversal/tree-boundary-traversal.cpp
+++ Difficulty: Medium/Tree Boundary Traversal/tree-boundary-traversal.cpp
@@ -18,47 +18,45 @@ class Node {
 class Solution {
   public:
   
-  bool isLeaf(Node * node){
-      return (node->left == NULL && node ->right == NULL);
+  bool isleaf(Node * root){
+      return (root->left == NULL && root->right == NULL);
   }
   
   void leftbound(Node * root , vector<int>&res){
-      Node * cur = root ->left;
-      while(cur){
-          if(!isLeaf(cur)) res.push_back(cur->data);
-          if(cur->left) cur = cur->left;
-          else cur = cur->right;
-      }
+    Node * cur = root->left;
+    while(cur){
+        if(!isleaf(cur)) res.push_back(cur->data);
+        if(cur->left!=NULL) cur = cur->left;
+        else cur = cur->right;
+    }
   }
   
   void addleaf(Node * root , vector<int>&res){
-      while(isLeaf(root)){
+      while(isleaf(root)){
           res.push_back(root->data);
           return;
       }
-      
       if(root->left) addleaf(root->left,res);
       if(root->right) addleaf(root->right,res);
   }
   
-  
-  void rightbound(Node * root , vector<int>&res){
-      Node * cur = root -> right;
+  void rightbound(Node * root, vector<int>&res){
+      Node * cur = root->right;
       vector<int>temp;
       while(cur){
-          if(!isLeaf(cur)) temp.push_back(cur->data);
-          if(cur->right) cur = cur->right;
+          if(!isleaf(cur))temp.push_back(cur->data);
+          if(cur->right!=NULL) cur = cur->right;
           else cur = cur->left;
       }
-      for(int i =temp.size()-1 ; i>=0 ;i--){
+      for(int i = temp.size()-1;i>=0;i--){
           res.push_back(temp[i]);
-      }
+  }
   }
   
     vector<int> boundaryTraversal(Node *root) {
         vector<int>res;
         if(!root) return res;
-        if(!isLeaf(root)) res.push_back(root->data);
+        if(!isleaf(root)) res.push_back(root->data);
         leftbound(root,res);
         addleaf(root,res);
         rightbound(root,res);
