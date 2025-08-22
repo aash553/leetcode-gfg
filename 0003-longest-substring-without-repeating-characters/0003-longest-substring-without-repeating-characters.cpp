9d77762c7a989087fca0557d3af6df4a06f9a59e
Aashish <122160985+aash553@users.noreply.github.com>
2025-08-22 22:07:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int l = 0; 
-        int r = 0;
-        int maxi = 0 ;
+        int l =0;
+        int r =0;
         int hash[256];
         fill(hash,hash+256,-1);
         int n = s.length();
+        int maxi = 0 ;
 
         if(n<=1) return n;
-
         while(r<n){
-        
-            if(hash[s[r]]!=-1 && hash[s[r]] >= l) 
-            // it means its present 
-            {
-                    l=hash[s[r]]+1;
+
+            if(hash[s[r]] != -1 || hash[s[r]] >= l){
+               l = hash[s[r]]+1;
             }
             int len = r-l+1;
             maxi = max(maxi,len);
-            hash[s[r]] = r;
+            hash[s[r]]=r;
             r++;
         }
         return maxi;
