7b1eefd3157de6c0ade1183b5a828d456e6a7b85
Aashish <122160985+aash553@users.noreply.github.com>
2025-08-22 15:46:10 +0530

    Time: 3 ms (70.67%), Space: 15.1 MB (10.74%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -5,10 +5,8 @@ public:
         for(int i =0;i<nums.size();i++){
             int a = nums[i];
             int need = target - a;
-            if(mpp.find(need) != mpp.end()){
-                return {mpp[need],i};
-            }
-                mpp[a] = i;
+            if(mpp.find(need)!=mpp.end()) return {mpp[need],i};
+            mpp[a]=i;
         }
         return {-1,-1};
     }
