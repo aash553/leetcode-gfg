94cb8274e0f1348e6b9e7e545d7852efef54ab8b
Aashish <122160985+aash553@users.noreply.github.com>
2025-08-23 16:25:21 +0530

    Time: 11 ms (91.65%), Space: 25 MB (66.58%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -3,21 +3,14 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string,vector<string>>mp;
 
-        for(string s: strs){
-            vector<int>freq(26,0);
-            for(char c : s){
-            freq[c-'a']++;
-            }
-
-            //to conver freq to stirng
-            string key;
-            for(int f: freq){
-                key += "#" + to_string(f);
-            }
-            mp[key].push_back(s);
+        for(auto x : strs){
+            string key = x;
+            sort(key.begin(),key.end());
+            mp[key].push_back(x);
         }
+
         vector<vector<string>>ans;
-        for(auto &it : mp){
+        for(auto &it :mp){
             ans.push_back(it.second);
         }
         return ans;
