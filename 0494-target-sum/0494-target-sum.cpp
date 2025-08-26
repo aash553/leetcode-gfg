ae2d4cb0562bc9338c10746d070812e20b618bf9
Aashish <122160985+aash553@users.noreply.github.com>
2025-08-26 22:12:55 +0530

    Time: 6 ms (71.56%), Space: 15.7 MB (38.96%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
 
+    vector<vector<int>>t;
     int solve(vector<int>& nums, int i , int point){
         int n = nums.size();
         //base case 
@@ -8,18 +9,25 @@ public:
             return (point==0) ? 1 :0;
         }
 
-        int include = solve(nums,i+1,point-nums[i]);
+        if (point < 0) return 0;
+
+        if(t[i][point] != -1) return t[i][point];
+         int include = 0;
+        if (nums[i] <= point) {
+            include = solve(nums, i+1, point - nums[i]);
+        }
         int exclude = solve(nums,i+1,point);
 
-        return include + exclude;
+        return t[i][point] = include + exclude;
     }
 
     int findTargetSumWays(vector<int>& nums, int target) {
         int n= nums.size();
         int total = 0;
         for(int n : nums) total += n;
-        if((total+target) %2 !=0) return 0;
+        if((total+target) %2 !=0 || abs(target) > total) return 0;
         int point = (total + target)/2;
+        t.assign(n+1,vector<int>(point+1,-1));
         return solve(nums,0,point);
     }
 };
