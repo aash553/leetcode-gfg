50e7db302d2d482a2ca681b0892431aeb6e6bb78
Aashish <122160985+aash553@users.noreply.github.com>
2025-08-30 17:47:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+  
+  vector<vector<int>>t;
+  
+  int solve(vector<int>&arr , int i ,int j){
+            // code here
+        int n = arr.size();
+        if(i>=j) return 0;
+        if(t[i][j] != -1) return t[i][j];
+        int mini = INT_MAX;
+        for(int k = i ; k<j;k++){
+            int temp = solve(arr,i,k) + solve(arr,k+1,j) + arr[i-1]*arr[k]*arr[j]; 
+        if(temp<mini){
+            mini = temp;
+        }
+        }
+       return t[i][j] = mini;
+  }
+    int matrixMultiplication(vector<int> &arr) {
+        int n = arr.size();
+         t.assign(n+1, vector<int>(n+1, -1));
+        return solve(arr,1,n-1);
+    }
+};
