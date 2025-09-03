87d35169f0c63a7d6df09a3cb7f2b3bd611fdcf7
Aashish <122160985+aash553@users.noreply.github.com>
2025-09-03 20:54:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -1,48 +1,48 @@
 class Solution {
-    
-    
-  private:
+  public:
   
-  bool detect(int src , vector<int>adjLs[] , int vis[]){
-      vis[src]=1;
+  bool detect(int node ,  vector<vector<int>>&adjls ,vector<int>&vis){
       queue<pair<int,int>>q;
-      q.push({src,-1});
+      vis[node] =1;
+      q.push({node,-1});
+      
       while(!q.empty()){
-          int node = q.front().first;
+          int actualnode = q.front().first;
           int parent = q.front().second;
           q.pop();
           
-          
-          for(auto adjacentlist : adjLs[node]){
+          for(auto adjacentlist : adjls[actualnode]){
               if(!vis[adjacentlist]){
-                  vis[adjacentlist]=1;
-                  q.push({adjacentlist,node});
-              }
-              else if (parent != adjacentlist){
+                  vis[adjacentlist] = 1;
+                  q.push({adjacentlist,actualnode});
+              }else if(parent != adjacentlist){
                   return true;
               }
           }
       }
       return false;
   }
-    
-    
-  public:
+  
+  
+  
     bool isCycle(int V, vector<vector<int>>& edges) {
-        vector<int>adjLs[V];
-        for( auto &e : edges){
-            int v = e[0];
-            int u = e[1];
-            adjLs[v].push_back(u);
-            adjLs[u].push_back(v);
+        // Code here
+        //this is for the adjacenty list
+        vector<vector<int>>adjls(V);
+        for(auto e : edges){
+            int u = e[0];
+            int v = e[1];
+            adjls[v].push_back(u);
+            adjls[u].push_back(v);
         }
         
-        int vis[V] = {0};
-        for(int i=0;i<V;i++){
+        vector<int>vis(V,0);
+        
+        for(int i = 0;i<V;i++){
             if(!vis[i]){
-            if(detect(i,adjLs,vis)){
-                return true;
-            }
+                if(detect(i,adjls,vis)){
+                    return true;
+                }
             }
         }
         return false;
