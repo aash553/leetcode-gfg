17522a0b7bb4802920d5217d6a26aeb7de8e8d1e
Aashish <122160985+aash553@users.noreply.github.com>
2025-09-06 15:19:02 +0530

    Time: 592 ms (21.41%), Space: 11.6 MB (91.85%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
 
-    int solve(vector<int>& nums, int i , int point){
+    int solve(vector<int>& nums,int i , int point){
         int n = nums.size();
-        //base case 
         if(i==n){
-            return (point==0) ? 1 :0;
+           return (point==0) ? 1 : 0;
         }
 
         int include = solve(nums,i+1,point-nums[i]);
@@ -13,13 +12,12 @@ public:
 
         return include + exclude;
     }
-
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n= nums.size();
+        int n = nums.size();
         int total = 0;
-        for(int n : nums) total += n;
-        if((total+target) %2 !=0) return 0;
-        int point = (total + target)/2;
+        for (int n : nums) total += n;
+        if((total+target) % 2 != 0) return 0;
+        int point = (total+target)/2;
         return solve(nums,0,point);
     }
 };
