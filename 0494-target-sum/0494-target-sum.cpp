7f64508cd22c436f329b9d8898ea659635a8a07d
Aashish <122160985+aash553@users.noreply.github.com>
2025-09-06 15:10:30 +0530

    Time: 602 ms (21.33%), Space: 11.5 MB (91.85%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
-    int findTargetSumWays(vector<int>& nums, int target) {
+
+    int solve(vector<int>& nums, int i , int point){
         int n = nums.size();
+        //base case 
+        if(i==n){
+            return (point==0) ? 1 :0;
+        }
+
+        int include = solve(nums,i+1,point-nums[i]);
+        int exclude = solve(nums,i+1,point);
+
+        return include + exclude;
+    }
+
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n= nums.size();
         int total = 0;
-        for(int i : nums) total += i;
-        if((total+target) % 2 != 0 || abs(target) > total) return 0;
+        for(int n : nums) total += n;
+        if((total+target) %2 !=0) return 0;
         int point = (total + target)/2;
-        vector<vector<int>>t(n+1,vector<int>(point+1,0));
-
-        t[0][0]= 1;
-        for(int i =1;i<=n;i++){
-            if(nums[i-1] ==0){
-                t[i][0] = 2*t[i-1][0];
-            }else{
-                t[i][0] = t[i-1][0];
-            }
-        }
-
-        for(int i =1;i<n+1;i++){
-            for(int j=1;j< point+1;j++){
-                if(nums[i-1] <= j){
-                    t[i][j] = t[i-1][j] + t[i-1][j-nums[i-1]];
-                }else{
-                    t[i][j] = t[i-1][j];
-                }
-            }
-        }
-       return t[n][point];
+        return solve(nums,0,point);
     }
 };
