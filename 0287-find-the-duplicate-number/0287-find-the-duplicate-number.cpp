632b5627a73ee9f9d63d174a15a0e7ee88e79f30
Aashish <122160985+aash553@users.noreply.github.com>
2025-09-12 14:45:53 +0530

    Time: 11 ms (45.6%), Space: 65.2 MB (42.75%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -4,13 +4,14 @@ public:
         int slow = 0;
         int fast = 0;
 
-        do{
+        while(true){
             slow = nums[slow];
             fast = nums[nums[fast]];
-        }while(slow!=fast);
-
-        slow =0;
-        while(slow!=fast){
+            if(slow == fast)
+            break;
+        }
+        slow = 0;
+        while(slow != fast){
             slow = nums[slow];
             fast = nums[fast];
         }
