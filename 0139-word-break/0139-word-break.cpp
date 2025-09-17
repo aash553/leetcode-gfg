bc960efd5c5b508825af0f5998307a5f7290f447
Aashish <122160985+aash553@users.noreply.github.com>
2025-09-17 22:01:31 +0530

    Time: 15 ms (34.11%), Space: 17.1 MB (28.8%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -2,26 +2,24 @@ class Solution {
 public:
 
     vector<int>t;
-    bool solve(string &s, int i , set<string>&st){
+    bool solve(string &s , int i ,  unordered_set<string>&st){
         int n = s.length();
-        if(i==s.length()){
+        //base case 
+        if(i==n){
             return true;
         }
-
         if(t[i] != -1) return t[i];
-            for(int j = i ; j<n;j++){
-                string sub = s.substr(i,j-i+1);
-                if(st.count(sub)){
-                    if(solve(s,j+1,st))
-                    return t[i] = true;
-                }
-    }
-            return t[i] = false;
+        for(int j = i ; j<n;j++){
+            string sub = s.substr(i,j-i+1);
+            if(st.count(sub) && solve(s,j+1,st))
+            return t[i] =  true;
+        }
+        return t[i] =  false;
     }
 
     bool wordBreak(string s, vector<string>& wordDict) {
         int n = s.length();
-        set<string>st;
+        unordered_set<string>st;
         for(auto it : wordDict){
             st.insert(it);
         }
