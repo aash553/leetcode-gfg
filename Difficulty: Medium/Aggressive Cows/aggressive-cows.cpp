f62bd8fa89d261b89081e792d4e50ec4914f5498
Aashish <122160985+aash553@users.noreply.github.com>
2025-09-19 16:44:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Aggressive Cows/aggressive-cows.cpp
+++ Difficulty: Medium/Aggressive Cows/aggressive-cows.cpp
@@ -1,41 +1,43 @@
 class Solution {
   public:
   
+  bool ismaxi(vector<int>&stalls , int mid , int k){
+      int cowcnt = 1;
+      int lastpos = stalls[0];
+      
+      for(int i = 0;i<stalls.size();i++){
+          if(stalls[i] - lastpos >= mid){
+              cowcnt ++;
+              if(cowcnt == k){
+                  return true;
+              }
+              lastpos = stalls[i];
+          }
+      }
+      return false;
+  }
   
-   bool ismaxi(vector<int> &stalls, int k, int mid){
-    int cowcnt = 1;
-    int lastpos = stalls[0];
-    
-    for(int i =0;i<stalls.size();i++){
-        if(mid<=stalls[i]-lastpos){
-            cowcnt++;
-            if(cowcnt == k ){
-                return true;
-            }
-            lastpos = stalls[i];
-        }
-    }
-    return false;
-   }
+  
     int aggressiveCows(vector<int> &stalls, int k) {
+        // code here
         sort(stalls.begin(),stalls.end());
         int n = stalls.size();
         int s = 0;
-        int ans = 0;
         int maxi = INT_MIN;
-        for(int i =0;i<n;i++){
-            maxi= max(maxi,stalls[i]);
+        int ans = 0;
+        for(int i=0; i<n;i++){
+            maxi = max(maxi,stalls[i]);
         }
         int e = maxi;
         
         while(s<=e){
-            int mid = s + (e-s)/2;
-            if(ismaxi(stalls,k,mid)){
-                ans = mid;
-                s = mid + 1 ;
-            }
-            else{
-                e = mid -1;
+            int mid = s+(e-s)/2;
+            
+            if(ismaxi(stalls,mid,k)){
+                ans= mid;
+                s = mid +1;
+            }else{
+                e = mid - 1;
             }
         }
         return ans;
