fe34c68d4f1d22477e1a19ee7ab33be1747c6a7d
Aashish <122160985+aash553@users.noreply.github.com>
2025-09-19 16:17:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.cpp
+++ Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.cpp
@@ -1,39 +1,42 @@
-class Solution {
+class Solution
+{
   public:
   
-  int minpages(vector<int>&arr, int pages ){
+  
+  int minpages(vector<int>&arr, int mid){
+      int n = arr.size();
       int student = 1;
-      int pagestudent = 0;
-      
-      for(int i =0; i<arr.size();i++){
-        if(pagestudent + arr[i] <= pages){
-            pagestudent += arr[i];
-        }else{
-            student++;
-            pagestudent = arr[i];
-        }
+      int pagestudent = 0; 
+      for(int i =0;i<n;i++){
+          if(arr[i]+ pagestudent <= mid){
+              pagestudent += arr[i];
+          }else {
+              student ++;
+              pagestudent = arr[i];
+          }
       }
       return student;
   }
+  
   
     int findPages(vector<int> &arr, int k) {
         int n = arr.size();
         if(k>n) return -1;
         int low = *max_element(arr.begin(),arr.end());
         int high = 0;
-        for(int i =0;i<arr.size();i++){
+        for(int i =0 ;i<n;i++){
             high += arr[i];
         }
         
-        while(low<=high){
-            int mid = low +(high-low)/2;
+        while(low <= high){
+            int mid = low+(high-low)/2;
+            int student = minpages(arr,mid);
             
-            int students = minpages(arr,mid);
-            if(students > k){
+            if(student > k){
                 low = mid + 1;
             }
-            else{
-                high = mid -1 ;
+            else {
+                high = mid - 1 ;
             }
         }
         return low;
