6b45afec5ae46da2cc7e586912a3170d3f3de742
Aashish <122160985+aash553@users.noreply.github.com>
2025-09-25 13:37:42 +0530

    Time: 7 ms (59.93%), Space: 13.2 MB (49.06%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    vector<vector<int>>t;
-    int solve(int i , int j , string &word1, string &word2){
-    //base case 
-    if(i<0) return j+1;
-    if(j<0) return i+1;
-
-    if(t[i][j] != -1) return t[i][j]; 
-    if(word1[i] == word2[j]){
-        return t[i][j] =  solve(i-1,j-1,word1,word2);
-    }else{
-        return t[i][j] = 1+ min({
-            solve(i,j-1,word1,word2),
-            solve(i-1,j,word1,word2) ,
-            solve(i-1,j-1,word1,word2)
-        });
-    }
-    }
-
-
+   
     int minDistance(string word1, string word2) {
         int n = word1.length();
         int m = word2.length();
-        t.assign(n+1,vector<int>(m+1,-1));
-        return solve(n-1,m-1,word1,word2);
+       vector<vector<int>>t(n+1,vector<int>(m+1,-1));
+
+        for (int i = 0; i <= n; i++) t[i][0] = i;
+        for (int j = 0; j <= m; j++) t[0][j] = j;
+
+
+        for(int i =1;i<n+1;i++){
+            for(int j = 1 ;j<m+1 ; j++){
+                if(word1[i-1] == word2[j-1]){
+                    t[i][j] = t[i-1][j-1];
+                }else {
+                   t[i][j]  = 1 + min({
+                        t[i][j-1],
+                        t[i-1][j],
+                        t[i-1][j-1]
+                    });
+                }
+            }
+        }
+        return t[n][m];
     }
 };
