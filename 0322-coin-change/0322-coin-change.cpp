b3b7ea13fbdfea9f6a3dee3ce357e914bb17c246
Aashish <122160985+aash553@users.noreply.github.com>
2025-09-27 11:59:33 +0530

    Time: 57 ms (16.53%), Space: 53.8 MB (9.89%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,35 +1,24 @@
 class Solution {
 public:
+
+    vector<vector<int>>t;
+    int solve(vector<int>& coins, int i , int amount){
+        //base case 
+        int n = coins.size();
+        if(i==n) return 1e9;
+        if(amount == 0) return 0;
+        if(amount < 0 ) return 1e9;
+        if(t[i][amount] != -1) return t[i][amount];
+        int pick = 0;
+        pick = 1+solve(coins, i , amount - coins[i]);
+        int notpick = solve(coins, i+1, amount);
+
+        return t[i][amount] =  min(pick,notpick);
+    }
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n+1, vector<int>(amount+1, 0));
-
-        // base case
-        for(int j=0; j<=amount; j++) {
-            dp[0][j] = 1e9; // cannot make sum with 0 coins
-        }
-        dp[0][0] = 0;
-
-        // fill the second row (only first coin allowed)
-        for(int j=0; j<=amount; j++) {
-            if(j % coins[0] == 0) {
-                dp[1][j] = j / coins[0];
-            } else {
-                dp[1][j] = 1e9;
-            }
-        }
-
-        // rest of the table
-        for(int i=2; i<n+1; i++) {
-            for(int j=0; j<amount+1; j++) {
-                if(coins[i-1] <= j) {
-                    dp[i][j] = min(1 + dp[i][j - coins[i-1]], dp[i-1][j]);
-                } else {
-                    dp[i][j] = dp[i-1][j];
-                }
-            }
-        }
-
-        return dp[n][amount] >= 1e9 ? -1 : dp[n][amount];
+        t.assign(n+1,vector<int>(amount+1,-1));
+        int res = solve(coins , 0 , amount);
+        return (res >= 1e9) ? -1 : res;
     }
 };
