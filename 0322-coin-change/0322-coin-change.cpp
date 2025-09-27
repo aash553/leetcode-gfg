4b2d2a9b832d3a483ff7445b6ee2489497004609
Aashish <122160985+aash553@users.noreply.github.com>
2025-09-27 12:17:34 +0530

    Time: 48 ms (30.32%), Space: 52.9 MB (14.93%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-
-    vector<vector<int>>t;
-    int solve(vector<int>& coins, int i , int amount){
-        //base case 
-        int n = coins.size();
-        if(i==n) return 1e9;
-        if(amount == 0) return 0;
-        if(amount < 0 ) return 1e9;
-        if(t[i][amount] != -1) return t[i][amount];
-        int pick = 0;
-        pick = 1+solve(coins, i , amount - coins[i]);
-        int notpick = solve(coins, i+1, amount);
-
-        return t[i][amount] =  min(pick,notpick);
-    }
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        t.assign(n+1,vector<int>(amount+1,-1));
-        int res = solve(coins , 0 , amount);
-        return (res >= 1e9) ? -1 : res;
+        int INF = 1e9;
+      vector<vector<int>>t(n+1,vector<int>(amount+1,INF));
+
+      //initialization 
+      for(int i =0;i<=n;i++){
+      t[i][0] = 0;
+      }
+
+      for(int i = 1;i<n+1;i++){
+        for(int j =1;j<amount+1;j++){
+            int notpick = t[i-1][j];
+            int pick = INF;
+            if(j >= coins[i-1]){
+                pick = 1+ t[i][j-coins[i-1]];
+            }
+            t[i][j] = min(pick,notpick);
+        }
+      }
+      return (t[n][amount] >= INF) ? -1 : t[n][amount];
     }
 };
