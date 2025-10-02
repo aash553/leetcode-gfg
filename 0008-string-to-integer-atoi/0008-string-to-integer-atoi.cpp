fea7f988598bae6e8243e9cb8c3706b5e6cafcf5
Aashish <122160985+aash553@users.noreply.github.com>
2025-10-02 13:13:02 +0530

    Time: 1 ms (23.39%), Space: 9.1 MB (55.58%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        int i = 0;
-        int n = s.length();
-        long ans = 0;
+        int n = s.size();
+        int i =0;
+
+        //skipping space 
+        while(i<n && s[i] == ' ') i++;
+
+        //sign
         int sign = 1;
-
-        // Step 1: Skip leading whitespaces
-        while(i < n && s[i] == ' ') i++;
-
-        // Step 2: Check for optional '+' or '-'
-        if(i < n && (s[i] == '+' || s[i] == '-')) {
-            sign = (s[i] == '-') ? -1 : 1;
+        if(i<n && (s[i] == '+' || s[i] == '-')){
+            if(s[i]== '-') sign = -1; 
             i++;
         }
 
-        while(i < n && isdigit(s[i])) {
-            ans = ans * 10 + (s[i] - '0');
+        //traversing
+        long num = 0;
+        while(i<n && isdigit(s[i])){
+            num = num * 10 + (s[i] - '0');
 
-            // this is for overfllow
-            if(sign == 1 && ans > INT_MAX) return INT_MAX;
-            if(sign == -1 && -1 * ans < INT_MIN) return INT_MIN;
+            if(sign * num > INT_MAX ) return INT_MAX;
+            if(sign * num < INT_MIN) return INT_MIN;
 
             i++;
         }
-
-        return (int)(sign * ans);
+        return (int)(sign * num);
     }
 };
