218ce376c5a150b0f1f1f2706cd94811f23b416e
Aashish <122160985+aash553@users.noreply.github.com>
2025-10-06 08:46:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Bottom View of Binary Tree/bottom-view-of-binary-tree.cpp
+++ Difficulty: Medium/Bottom View of Binary Tree/bottom-view-of-binary-tree.cpp
@@ -1,37 +1,38 @@
 /*
-struct Node
-{
+class Node {
+public:
     int data;
     Node* left;
     Node* right;
+
+    Node(int x) {
+        data = x;
+        left = right = NULL;
+    }
 };
 */
 
 class Solution {
   public:
     vector<int> bottomView(Node *root) {
-        vector<int>ans;
-        if(root==NULL) return ans;
-        map<int,int>mpp;
-        queue<pair<Node* , int>>q;
+        vector<int>bottom;
+        if(root == NULL) return bottom;
+        map<int,int>mpp; // vertical , node val
+        queue<pair<Node*,int>>q;
         q.push({root,0});
         while(!q.empty()){
             auto it = q.front();
             q.pop();
             Node * node = it.first;
-            int line =it.second;
-            mpp[line] = node->data;
+            int line = it.second;
+            mpp[line] = node -> data;
             
-            if(node->left!=NULL){
-                q.push({node->left , line-1});
-            }
-            if(node->right!=NULL){
-                q.push({node->right, line+1});
-            }
+            if(node -> left != NULL) q.push({node->left , line-1});
+            if(node ->right != NULL) q.push({node->right , line+1});
         }
         for(auto it : mpp){
-            ans.push_back (it.second);
+            bottom.push_back(it.second);
         }
-        return ans;
+        return bottom;
     }
 };
