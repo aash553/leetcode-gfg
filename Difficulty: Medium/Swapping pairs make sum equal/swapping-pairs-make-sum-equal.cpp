31e6a0719f8d62b53acec1eed2c5f6fcaf864474
Aashish <122160985+aash553@users.noreply.github.com>
2025-11-04 10:24:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Swapping pairs make sum equal/swapping-pairs-make-sum-equal.cpp
+++ Difficulty: Medium/Swapping pairs make sum equal/swapping-pairs-make-sum-equal.cpp
@@ -2,28 +2,27 @@ class Solution {
 
   public:
     bool findSwapValues(vector<int>& a, vector<int>& b) {
-        int cnt1=0 , cnt2=0;
-        for(int i =0;i<a.size();i++){
+        int cnt1 = 0 , cnt2 = 0;
+        for(int i = 0 ; i<a.size() ; i++){
             cnt1 += a[i];
         }
-        for(int i =0;i<b.size();i++){
-            cnt2 +=b[i];
+        for(int i = 0 ;i <b.size() ; i++){
+            cnt2 += b[i];
         }
         
         int diff = cnt1-cnt2;
         
-        if(diff %2 !=0) return false;
+        if(diff % 2 != 0) return false;
         
-        int target = diff/2;
+        int target = diff / 2;
         
-        unordered_set<int> setb(b.begin(),b.end());
+        unordered_set<int>setb(b.begin() , b.end());
         
-        for(int i =0;i<a.size();i++){
-            int need = a[i]-target;
+        for(int i = 0; i<a.size(); i ++){
+            int need = a[i] - target;
             
-            if(setb.count(need)){
-                return true;
-            }
+            if(setb.count(need))
+            return true;
         }
         return false;
     }
