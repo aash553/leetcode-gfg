959bffa646d5b5401bf9773705bd14de69e02872
Aashish <122160985+aash553@users.noreply.github.com>
2025-11-04 12:38:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count Inversions/count-inversions.cpp
+++ Difficulty: Medium/Count Inversions/count-inversions.cpp
@@ -1,19 +1,20 @@
 class Solution {
   public:
   
-  int merge(vector<int>&arr,int low , int mid , int high ){
+  int merge(vector<int>&arr , int low , int high , int mid){
       int left = low;
-      int right = mid + 1;
+      int right = mid+1;
+      int cnt = 0 ;
       vector<int>temp;
-      int cnt = 0;
       while(left <= mid && right <= high){
           if(arr[left] <= arr[right]){
-              temp.push_back(arr[left]);
-              left++;
-          }else{
-              temp.push_back(arr[right]);
-              cnt += (mid-left+1);
-              right++;
+            temp.push_back(arr[left]);
+            left++;
+          }
+          else{
+            temp.push_back(arr[right]);
+            cnt += (mid-left+1);
+            right++;
           }
       }
       while(left <= mid){
@@ -24,25 +25,26 @@ class Solution {
           temp.push_back(arr[right]);
           right++;
       }
+      
       for(int i = low ; i <= high ; i++){
           arr[i] = temp[i-low];
       }
       return cnt;
   }
   
-  int mergesort(vector<int>&arr, int low , int high){
-      //base case 
+  int solve(vector<int>&arr, int low , int high){
       int cnt = 0;
-      if(low >= high)return cnt;
-      int mid = low + (high-low)/2;
-      cnt += mergesort(arr,low,mid);
-      cnt += mergesort(arr,mid+1,high);
-      cnt += merge(arr,low,mid,high);
+      if(low >= high) return cnt;
+      int mid = low +(high-low)/2;
+      cnt += solve(arr,low,mid);
+      cnt += solve(arr,mid+1,high);
+      cnt += merge(arr,low,high,mid);
       return cnt;
   }
   
     int inversionCount(vector<int> &arr) {
-        int n = arr.size();
-       return mergesort(arr,0,n-1);
+        // Code Here
+        int n= arr.size();
+        return solve(arr, 0 , n-1);
     }
 };
