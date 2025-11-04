4c2573545af1015d929ea3f84f216f7b3bca6afb
Aashish <122160985+aash553@users.noreply.github.com>
2025-11-04 09:50:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum swaps and K together/minimum-swaps-and-k-together.cpp
+++ Difficulty: Medium/Minimum swaps and K together/minimum-swaps-and-k-together.cpp
@@ -3,31 +3,32 @@ class Solution {
   public:
     int minSwap(vector<int>& arr, int k) {
         // code here
-        int n = arr.size();
-        int bad = 0;
-        int cnt = 0;
-        int ans = 0;
-        for ( int i =0;i<n;i++){
-            if(arr[i]<=k)
-                cnt++;
+        int ans = 0 ;
+        int bad = 0 ;
+        int cnt = 0 ;
+        
+        for(int i = 0 ; i < arr.size(); i++){
+            if(arr[i] <= k){
+                cnt ++;
+            }
         }
         
-        for(int i = 0 ; i<cnt;i++){
-            if(arr[i]>k)
-            bad++;
+        for(int i = 0;i<cnt ; i++){
+            if(arr[i] > k){
+                bad ++;
+            }
         }
-        
         ans = bad;
         
-        for(int i =0 , j = cnt ; j<n ; i++,j++){
-            if(arr[i]>k)
-            bad--;
-            
-            if(arr[j]>k)
-            bad++;
-            
+        for(int i = 0 ,  j = cnt ; j<arr.size() ; i++ , j++){
+            if(arr[i] > k){
+                bad--;
+            }
+            if(arr[j] > k){
+                bad ++ ;
+            }
             ans = min(ans,bad);
         }
-    return ans;
+        return ans;
     }
 };
