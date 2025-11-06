1b55c7631c3ef5dd3eab77a12823b63dc384a7aa
Aashish <122160985+aash553@users.noreply.github.com>
2025-11-06 11:09:00 +0530

    Time: 160 ms (66.66%), Space: 195 MB (53.52%) - LeetHub

--- 2461-amount-of-time-for-binary-tree-to-be-infected/2461-amount-of-time-for-binary-tree-to-be-infected.cpp
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/2461-amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -12,49 +12,15 @@
 class Solution {
 public:
 
-    int  maxi(unordered_map<TreeNode* ,TreeNode*> &parent_track , TreeNode* target){
-        unordered_map<TreeNode* , bool> vis;
+    TreeNode * mark(TreeNode * root , unordered_map<TreeNode* , TreeNode*>&parent_track , int start){
         queue<TreeNode*>q;
-        q.push(target);
-        vis[target]= true;
-        int maxi = 0;
-        while(!q.empty()){
-            int size = q.size();
-            int flag = 0;
-            for(int i=0;i<size;i++){
-                TreeNode * node = q.front();
-                q.pop();
-
-                if(node->left && !vis[node->left]){
-                    flag = 1;
-                    q.push(node->left);
-                    vis[node->left] = true;
-                }
-
-                if(node->right && !vis[node->right]){
-                    flag = 1;
-                    q.push(node->right);
-                    vis[node->right] = true;
-                }
-
-                if(parent_track[node] && !vis[parent_track[node]]){
-                    flag = 1;
-                    q.push(parent_track[node]);
-                    vis[parent_track[node]]= true;
-                }
-            }
-            if(flag)maxi++;
-        }
-        return maxi;
-    }
-    TreeNode* mark(TreeNode * root , unordered_map<TreeNode* ,TreeNode*>&parent_track, int start){
-        queue<TreeNode*>q;
+        TreeNode * res;
         q.push(root);
-        TreeNode * res ;
         while(!q.empty()){
             TreeNode * node = q.front();
-            if(node->val == start) res = node;
             q.pop();
+            if(node->val == start) res = node;
+
             if(node->left){
                 parent_track[node->left] = node;
                 q.push(node->left);
@@ -66,10 +32,43 @@ public:
         }
         return res;
     }
+
+    int maxi ( unordered_map<TreeNode*,TreeNode*> & parent_track , TreeNode * target){
+        queue<TreeNode*>q;
+        q.push(target);
+        unordered_map<TreeNode*, bool>vis;
+        vis[target] = true;
+        int time = 0;
+        while(!q.empty()){
+            int size = q.size();
+            int flag = 0 ;
+            for(int i = 0 ; i <size;i++){
+            TreeNode * node = q.front();
+            q.pop();
+            if(node->left && !vis[node->left]){
+                flag = 1;
+                q.push(node->left);
+                vis[node->left] = true;
+            }
+            if(node->right && !vis[node->right]){
+                flag = 1;
+                q.push(node->right);
+                vis[node->right] = true;
+            }
+            if(parent_track[node] && !vis[parent_track[node]]){
+                flag = 1;
+                q.push(parent_track[node]);
+                vis[parent_track[node]]= true;
+            }
+            }
+            if(flag)time++;
+        }
+        return time;
+    }
+
     int amountOfTime(TreeNode* root, int start) {
-        
-        unordered_map<TreeNode* ,TreeNode*>parent_track;
-       TreeNode * target =  mark(root , parent_track,start);
-        return maxi(parent_track , target);
+        unordered_map<TreeNode* , TreeNode* > parent_track;
+        TreeNode * target = mark(root,parent_track,start);
+        return maxi(parent_track,target);
     }
 };
