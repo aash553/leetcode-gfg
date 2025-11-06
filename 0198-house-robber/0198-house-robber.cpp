7456d685b73442bf2cfa8cd0d54a2e0149d8b412
Aashish <122160985+aash553@users.noreply.github.com>
2025-11-06 16:13:13 +0530

    Time: 0 ms (100%), Space: 10.6 MB (64%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
- 
+    vector<int>t;
+    int solve(vector<int>&nums  ,int n){
+        //base case 
+        if(n==0) return 0;
+        if(n == 1) return nums[0];
+
+        if(t[n] != -1) return t[n];
+        int pick = nums[n-1]+ solve(nums,n-2);
+        int notpick = solve(nums,n-1);
+
+        return t[n] =  max(pick,notpick);
+    }
+
     int rob(vector<int>& nums) {
-
         int n = nums.size();
-        int prev1 = nums[0];
-        int prev2= 0;
-
-        for ( int i =1;i<n; i++){
-
-            int take = nums[i];
-            if(i>1) take+=prev2;
-
-            int notake = 0 + prev1;
-
-            int curi = max(take,notake);
-            prev2=prev1;
-            prev1 =curi;
-        }
-        return prev1;
+        t.assign(n+1,-1);
+        return solve(nums,n);
     }
 };
