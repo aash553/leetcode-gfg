75e628e05f7c35d58acf24bf7fa785f06b5d62e7
Aashish <122160985+aash553@users.noreply.github.com>
2025-11-07 14:49:48 +0530

    Time: 3 ms (81.47%), Space: 11.6 MB (41.91%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-
         vector<int>result;
-        
-        if(s.length()<p.length()) return {};
 
-        vector<int>freq1(26,0) , freq2(26,0);
+        if(s.length() < p.length()) return {};
+        vector<int>freq1(26,0), freq2(26,0);
 
-        for(int i =0;i<p.size();i++){
+        for(int i = 0 ;i<p.size() ; i++){
+            freq1[p[i]-'a']++;
+            freq2[s[i]-'a']++;
+        }
+        if(freq1 == freq2) result.push_back(0);
 
-            freq1[s[i]-'a']++;
-            freq2[p[i]-'a']++;
-        } 
-        if(freq1==freq2) result.push_back(0);
-        for(int i =p.length();i<s.length();i++){
+        for(int i = p.length() ; i<s.length() ; i++){
+            freq2[s[i]-'a']++;
+            freq2[s[i-p.length()]-'a']--;
 
-            freq1[s[i]-'a']++;
-            freq1[s[i-p.length()]-'a']--;
-            if(freq1==freq2) result.push_back(i-p.length()+1);
+            if(freq1==freq2) result.push_back(i - p.length() +1);
         }
         return result;
     }
