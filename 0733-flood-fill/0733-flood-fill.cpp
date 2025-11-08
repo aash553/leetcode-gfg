dbb48353017c71b95712a95df1e6186f488b1907
Aashish <122160985+aash553@users.noreply.github.com>
2025-11-08 15:46:24 +0530

    Time: 0 ms (100%), Space: 18 MB (74.56%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        
-        vector<vector<int>>ans = image;
+
+    void dfs(int row , int col , vector<vector<int>>&ans , int inicolor ,vector<vector<int>>& image , int color ){
+
         int n = image.size();
         int m = image[0].size();
 
-        int inicolor = image[sr][sc];
-        if (inicolor == color) return ans;
+        ans[row][col] = color;
 
-        queue<pair<int,int>>q;
-        q.push({sr,sc});
-        ans[sr][sc] = color;
+        int delrow[] = {-1,1,0,0};
+        int delcol[] = {0,0,-1,1};
 
-        while(!q.empty()){
-            auto[row,col] = q.front();
-            q.pop();
+        for(int i = 0 ;i<4 ; i++){
+            int nrow = row + delrow[i];
+            int ncol = col + delcol[i];
 
-            int delrow[] = {-1,1,0,0};
-            int delcol[] = {0,0,-1,1};
-
-            for(int i = 0 ;i<4 ; i++){
-                int nrow = row+delrow[i];
-                int ncol = col+delcol[i];
-            if(nrow < n && nrow >= 0 && ncol < m && ncol>=0 && ans[nrow][ncol] == inicolor){
-                ans[nrow][ncol] = color;
-                q.push({nrow,ncol});
-            }
+            if(nrow<n && nrow >= 0 && ncol <m && ncol >= 0 && ans[nrow][ncol] == inicolor && ans[nrow][ncol] != color){
+                dfs(nrow,ncol,ans,inicolor,image,color);
             }
         }
+     }
+
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        //using dfs
+        vector<vector<int>>ans = image;
+        int inicolor = image[sr][sc];
+
+        dfs(sr,sc,ans,inicolor,image,color);
         return ans;
     }
 };
