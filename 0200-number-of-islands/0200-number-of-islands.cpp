4ddbe6016f0ac4f80cf3968ed21e86691bf98a16
Aashish <122160985+aash553@users.noreply.github.com>
2025-11-08 11:07:11 +0530

    Time: 20 ms (93.95%), Space: 17.3 MB (51.67%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,37 +1,35 @@
 class Solution {
 public:
-    void bfs(int row,int col,vector<vector<char>>& grid){
+
+    void dfs(int row , int col ,vector<vector<int>>&vis , vector<vector<char>>& grid ){
+        vis[row][col] = 1;
         int n = grid.size();
         int m = grid[0].size();
-        queue<pair<int,int>>q;
-        q.push({row,col});
 
-        while(!q.empty()){
-            int row = q.front().first;
-            int col = q.front().second;
-            q.pop();
-            int delrow[] = {-1,1,0,0};
-            int delcol[] = {0,0,1,-1};
-            for(int i =0;i<4;i++){
-                int nrow = row + delrow[i];
-                int ncol = col + delcol[i];
-                if(nrow>=0 && nrow<n && ncol >=0 && ncol <m && grid[nrow][ncol] == '1'){
-                    grid[nrow][ncol] = 0;
-                    q.push({nrow,ncol});
-                }
+        int delrow[] = {-1,1,0,0};
+        int delcol[] = {0,0,-1,1};
+
+        for(int i = 0 ; i<4;i++){
+            int nrow = row + delrow[i];
+            int ncol = col + delcol[i];
+
+            if(nrow >= 0 && nrow < n &&  ncol >= 0 && ncol < m  && !vis[nrow][ncol] && grid[nrow][ncol] == '1'){
+                dfs(nrow,ncol,vis,grid);
             }
         }
     }
+
     int numIslands(vector<vector<char>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        int cnt = 0 ;
-        //vector<vector<int>>vis(n,vector<int>(m,0));
-        for(int row = 0; row<n;row++){
-            for(int col = 0;col<m;col++){
-                if(grid[row][col] == '1'){
-                    cnt ++;
-                    bfs(row,col,grid);
+        int n = grid.size();//row
+        int m = grid[0].size(); //column
+
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        int cnt = 0;
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(!vis[i][j] && grid[i][j] == '1'){
+                    cnt++;
+                    dfs(i,j,vis,grid);
                 }
             }
         }
