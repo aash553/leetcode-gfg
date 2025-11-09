11b827b871a7ae91c547823119d58f68f87ff9fb
Aashish <122160985+aash553@users.noreply.github.com>
2025-11-09 10:40:35 +0530

    Time: 23 ms (83.8%), Space: 17.2 MB (53.76%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
 
-    void dfs(int row , int col ,vector<vector<int>>&vis , vector<vector<char>>& grid ){
-        vis[row][col] = 1;
+    void dfs(int row , int col , vector<vector<int>>&vis , vector<vector<char>>& grid){
+        vis[row][col] =1;
         int n = grid.size();
         int m = grid[0].size();
 
         int delrow[] = {-1,1,0,0};
         int delcol[] = {0,0,-1,1};
 
-        for(int i = 0 ; i<4;i++){
+        for(int i = 0 ;i<4 ;i++){
             int nrow = row + delrow[i];
             int ncol = col + delcol[i];
-
-            if(nrow >= 0 && nrow < n &&  ncol >= 0 && ncol < m  && !vis[nrow][ncol] && grid[nrow][ncol] == '1'){
+            if(nrow < n && nrow >=0 && ncol < m && ncol >= 0 && !vis[nrow][ncol] && grid[nrow][ncol] =='1'){
                 dfs(nrow,ncol,vis,grid);
             }
         }
     }
 
     int numIslands(vector<vector<char>>& grid) {
-        int n = grid.size();//row
-        int m = grid[0].size(); //column
+        
+        int n = grid.size();
+        int m = grid[0].size();
 
         vector<vector<int>>vis(n,vector<int>(m,0));
+
         int cnt = 0;
         for(int i = 0;i<n;i++){
             for(int j = 0;j<m;j++){
-                if(!vis[i][j] && grid[i][j] == '1'){
-                    cnt++;
+                if(!vis[i][j] && grid[i][j] == '1' ){
+                    cnt +=1;
                     dfs(i,j,vis,grid);
                 }
             }
