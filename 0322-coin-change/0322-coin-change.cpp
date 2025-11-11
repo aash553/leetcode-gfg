c950a1f2b3c5f42b0b0ec5063c9eb49c168dfc75
Aashish <122160985+aash553@users.noreply.github.com>
2025-11-11 22:55:02 +0530

    Time: 63 ms (11.87%), Space: 53.9 MB (9.69%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount) {
+
+    vector<vector<int>>t;
+    int solve(vector<int>&coins , int i , int amount){
+
         int n = coins.size();
-        int INF = 1e9;
-      vector<vector<int>>t(n+1,vector<int>(amount+1,INF));
+        if(i == n) return 1e9;
+        if(amount == 0 ) return 0 ;
+        if(amount < 0) return 1e9;
 
-      //initialization 
-      for(int i =0;i<=n;i++){
-      t[i][0] = 0;
-      }
+        if(t[i][amount] != -1) return t[i][amount];
 
-      for(int i = 1;i<n+1;i++){
-        for(int j =1;j<amount+1;j++){
-            int notpick = t[i-1][j];
-            int pick = INF;
-            if(j >= coins[i-1]){
-                pick = 1+ t[i][j-coins[i-1]];
-            }
-            t[i][j] = min(pick,notpick);
-        }
-      }
-      return (t[n][amount] >= INF) ? -1 : t[n][amount];
+        int include = 1 + solve(coins , i , amount-coins[i]);
+        int exclude = solve(coins,i+1,amount);
+
+        return t[i][amount] = min(include , exclude);
+
+
+    }
+
+    int coinChange(vector<int>& coins, int amount) {
+        //recursion 
+        int n = coins.size();
+        t.assign(n+1,vector<int>(amount+1,-1));
+        int ans =  solve(coins , 0 , amount);
+        return (ans >= 1e9) ? -1 : ans;
     }
 };
