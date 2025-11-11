4cca41590271c4203977aacc4fb01a42de84e6d1
Aashish <122160985+aash553@users.noreply.github.com>
2025-11-11 10:06:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0) return 0;
-        if ( n==1) return 1;
         int a = 0;
         int b = 1;
-        for ( int i=2;i<=n;i++){
-            int newnumber = a+b;
+        
+        for(int i = 1 ; i<n;i++){
+            int newnumber = a + b;
+
             a=b;
             b=newnumber;
         }
