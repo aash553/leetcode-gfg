4af8345b98c8a34915bcf20066eb585528a99bef
Aashish <122160985+aash553@users.noreply.github.com>
2025-11-15 19:33:59 +0530

    Time: 9 ms (57.21%), Space: 11.4 MB (82.73%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,41 +1,37 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
+        int r = 0;
+        int l = 0;
+        int start = 0;
         int n = s.length();
-        int m = t.length();
-        int start  = 0;
-        int count = 0;
         int minlen = INT_MAX;
-        int l = 0 , r =0;
-
-        if(n<m) return "";
-
-        map<char, int >mpp;
+        int count = 0;
+        unordered_map<char,int>mpp;
         for(auto ch : t){
             mpp[ch]++;
         }
-
         count = mpp.size();
-
         while(r<n){
-            if(mpp.find(s[r]) != mpp.end()){
+            if(mpp.find(s[r])!= mpp.end()){
                 mpp[s[r]]--;
-                if(mpp[s[r]] == 0)
+                if(mpp[s[r]] ==0)
                 count--;
             }
             while(count == 0){
-                if(r-l+1 < minlen){
-                    minlen = r-l+1;
-                    start = l;
+                int len = r-l+1;
+                if(len < minlen){
+                minlen = len;
+                start = l;
                 }
-                if(mpp.find(s[l]) != mpp.end()){
-                    mpp[s[l]]++;
-                    if(mpp[s[l]] == 1)
-                    count++;
-                }
-                l++;
+            if(mpp.find(s[l]) != mpp.end()){
+                mpp[s[l]]++;
+                if(mpp[s[l]] == 1)
+                count++;
             }
-            r++;
+            l++;
+        }
+        r++;
         }
         if(minlen == INT_MAX) return "";
         return s.substr(start,minlen);
