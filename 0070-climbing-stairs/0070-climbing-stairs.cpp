4431e547a9e1abf2dbfaf067870b241cf044585e
Aashish <122160985+aash553@users.noreply.github.com>
2025-11-16 22:15:58 +0530

    Time: 0 ms (100%), Space: 8.6 MB (34.4%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
 
+    vector<int>t;
+    int solve(int n ){
+        if(n <=1 ) return 1;
+        if(t[n] !=-1) return t[n];
+        int steps = solve(n-1) + solve(n-2);
+
+        return t[n] = steps;
+    }
+
     int climbStairs(int n) {
-       vector<int>t(n+1,0);
-        
-        t[0] = 1;
-        t[1] = 1;
-        for(int i =2;i<n+1;i++){
-            t[i] = t[i-1] + t[i-2];
-        }
-        return t[n];
+        t.assign(n+1,-1);
+        return solve(n);
     }
 };
