9def90c268abb98b3987fc8a4e8bd201d1d469a8
Aashish <122160985+aash553@users.noreply.github.com>
2026-02-25 15:56:48 +0530

    Time: 0 ms (100%), Space: 21.9 MB (11.79%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -4,10 +4,10 @@ public:
         int n = nums.size();
         int totalsum = n*(n+1)/2;
         int sum = 0;
-        for(int i =0;i<n;i++){
+        for(int i = 0 ;i<n;i++){
             sum += nums[i];
         }
-        int ans = totalsum - sum;
-        return ans;
+        int miss = totalsum - sum;
+        return miss;
     }
 };
