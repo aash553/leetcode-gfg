160ba530e028210133f967adcb70ca2253a37a76
Aashish <122160985+aash553@users.noreply.github.com>
2026-05-18 11:31:35 +0530

    Time: 3 ms (19.59%), Space: 71.7 MB (79.81%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -4,9 +4,10 @@ public:
         int n = nums.size();
         int curr_max = nums[0];
         int maxi_max = nums[0];
+
         for(int i =1;i<n;i++){
-            curr_max = max(nums[i],curr_max+nums[i]);
-            maxi_max = max(maxi_max , curr_max);
+            curr_max = max(nums[i],curr_max + nums[i]);
+            maxi_max = max(maxi_max, curr_max);
         }
         return maxi_max;
     }
