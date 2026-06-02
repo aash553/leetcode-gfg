0604a7d64d661d1a1a03ee32f6c2b43cce32d0eb
Aashish <122160985+aash553@users.noreply.github.com>
2026-06-03 02:50:21 +0530

    Time: 59 ms (20.42%), Space: 31.1 MB (20.37%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        //first we will sort the given array and also keep i as constant
         int n = nums.size();
         vector<vector<int>>ans;
-        sort(nums.begin(),nums.end());
+        sort(nums.begin(), nums.end());
         for(int i = 0;i<n;i++){
-            //also we know that if the i == i +1 then we must continue till we find a differnt value for it ot avoid duplication
-            if(i>0 && nums[i] == nums[i-1])continue;
+            if(i > 0 && nums[i] == nums[i-1]) continue;
             int j = i+1;
             int k = n-1;
-            // we move till j<k 
             while(j<k){
-                int sum = nums[i]+nums[j]+nums[k];
-                if(sum < 0){
-                    j++;
-                }else if(sum > 0){
-                    k--;
-                }else{
-                    vector<int>temp = {nums[i],nums[j],nums[k]};
+                int sum = nums[i];
+                sum+=nums[j];
+                sum+=nums[k];
+                if(nums[i] + nums[j] + nums[k] ==0){
+                    vector<int>temp = {nums[i] , nums[j] , nums[k]};
                     ans.push_back(temp);
                     j++;
                     k--;
-                    //once we move j++ and k-- after every iteration we must make sure its not equal to their previous value so we move them until we find a different value for them to avoid duplication of triplets and make sure j<k
-                    while(j<k && nums[j] == nums[j-1]) j++;
-                    while(j<k && nums[k] == nums[k+1]) k--;
+                    while(j<k && nums[j] == nums[j-1])j++;
+                    while(j<k && nums[k] == nums[k+1])k--;
+                }else if( sum < 0){
+                    j++;
+                }else{
+                    k--;
                 }
             }
         }
